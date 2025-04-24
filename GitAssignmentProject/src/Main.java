5349df418e4eaed83c0fb3a58c11f40a67d1b98f
Siphobhengu394 <Siphobhengu394@gmail.com>
2025-04-24 12:09:24 +0200

    Changed a  message in Main on main branch

--- GitAssignmentProject/src/Main.java
+++ GitAssignmentProject/src/Main.java
@@ -1,6 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello NetBeans. Welcome to version control using Git");
+        System.out.println("Hello from the MAIN branch!");
+
     }
 }
